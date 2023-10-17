f5145790d2f7e841d99accfa985e3149e8e8db0f
PerilFlower <whoviancentral10@gmail.com>
2023-10-17 17:27:56 -0500

    adding to site

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,9 +5,11 @@ export default state => html`
     <h1>TRENDING</h1>
     <h1>LIBRARY</h1>
   </div>
-  <h3>
-    The weather in ${state.weather.city} is ${state.weather.description}.
-    Temperature is ${state.weather.temp}F, and it feels like
-    ${state.weather.feelsLike}F.
-  </h3>
+  <div id="weather">
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
+  </div>
 `;
