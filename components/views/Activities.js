9aad10f50ee32d98f6f9dc5d54b0641415de5110
PerilFlower <whoviancentral10@gmail.com>
2023-10-27 00:18:57 -0500

    adding work 10/26

--- components/views/Activities.js
+++ components/views/Activities.js
@@ -1,8 +1,15 @@
 import html from "html-literal";
 
 export default state => html`
-  <h1>
-    This is the activities page
-    <h1></h1>
-  </h1>
+  <div id="labels">
+    <h1>Activities</h1>
+  </div>
+  <div id="smalllabel">
+    <h6>click below to see all of our activities</h6>
+</div>
+    <h1>
+      This is the activities page
+      <h1></h1>
+    </h1>
+  </div>
 `;
